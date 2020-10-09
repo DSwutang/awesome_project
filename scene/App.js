ea02d6b012ab88e73528828417f9bd5243c7c674
唐李源 <2018011497@secoder.net>
2020-10-09 09:13:38 +0800

    登陆界面基本完成

---
+++ scene/App.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+//引入三个界面文件，并设置引入的类
+import HomeScene from './homeScene';
+import LoginScene from './loginScene';
+import RegisterScene from './registerScene';
+
+const Stack = createStackNavigator();
+
+function SimpleApp() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ gestureEnabled: true }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={LoginScene}
+          options={{ headerTitle: <Text>登陆</Text> }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScene}
+          initialParams={{ header: null }}
+        />
+        <Stack.Screen
+          name="register"
+          component={RegisterScene}
+          options={{ headerTitle: <Text>注册</Text> }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default class App extends React.Component {
+  render() {
+    return <SimpleApp />;
+  }
+}
+
+// export default App
