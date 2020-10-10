43684e66ac3d50badcbcb546a7f091a66262f579
唐李源 <2018011497@secoder.net>
2020-10-10 23:41:52 +0800

    fix: finish ci settings, and add test
    try
    
    fix #1

--- scene/photoScene.js
+++ scene/photoScene.js
@@ -1,2 +1,14 @@
 import React, {Component} from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  content: {
+    fontSize: 40,
+  },
+});
