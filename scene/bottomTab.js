a7bcb5bcbb73f178a011039191550e05874701ad
唐李源 <2018011497@secoder.net>
2020-10-13 10:37:01 +0800

    fix: delete useless file, end router
    fix #1

--- scene/bottomTab.js
+++ scene/bottomTab.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import routes from './router/index';
 
 import PhotoScene from './photoScene';
 import DeviceManagerScene from './deviceManagerScene';
@@ -16,14 +13,6 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <Tab.Navigator>
-      {/* {routes.map((router, index) => (
-          <Tab.Screen
-            name={router.name}
-            component={router.component}
-            key={index}
-            options={router.options}
-          />
-        ))} */}
       <Tab.Screen name="photo" component={PhotoScene} />
       <Tab.Screen name="device" component={DeviceManagerScene} />
       <Tab.Screen name="person" component={PersonManagerScene} />
