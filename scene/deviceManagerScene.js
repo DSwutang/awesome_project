44bac1989f6c8735aa3e59cff744f166d43b42b1
唐李源 <2018011497@secoder.net>
2020-10-25 23:21:17 +0800

    feat: add scroll list, cannot click yet

--- scene/deviceManagerScene.js
+++ scene/deviceManagerScene.js
@@ -1,5 +1,39 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, Button, Alert, TextInput} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  StatusBar,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
+import {TouchableHighlight} from 'react-native-gesture-handler';
+
+const DATA = [
+  {
+    name: '东门',
+    id: 1,
+  },
+  {
+    name: '南门',
+    id: 2,
+  },
+  {
+    name: '西门',
+    id: 3,
+  },
+  {
+    name: '北门',
+    id: 4,
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default class DeviceManagerScene extends Component {
   token = '';
@@ -8,10 +42,6 @@ export default class DeviceManagerScene extends Component {
   backToLogin = () => {
     const {goBack} = this.props.navigation; //获取navigation的goBack方法
     goBack(); //返回上一界面
-  };
-
-  onIDChanged = (new_ID) => {
-    this.input_id = new_ID;
   };
 
   bind = () => {
@@ -41,21 +71,16 @@ export default class DeviceManagerScene extends Component {
   render() {
     //console.log(this.props.route);
     this.token = this.props.route.params.token;
+
+    const renderItem = ({item}) => <Item title={item.name} />;
     return (
-      <View style={styles.container}>
-        <Text style={styles.content}>点击绑定设备</Text>
-        <View style={styles.inputBox}>
-          <TextInput
-            onChangeText={this.onIDChanged} //添加值改变事件
-            autoCapitalize="none" //设置首字母不自动大写
-            underlineColorAndroid={'transparent'} //将下划线颜色改为透明
-            placeholderTextColor={'#ccc'} //设置占位符颜色
-            placeholder={'设备号'} //设置占位符
-            ref="input_id"
-          />
-        </View>
-        <Button onPress={this.bind} style={styles.button} title="绑定设备" />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
     );
   }
 }
@@ -63,11 +88,15 @@ export default class DeviceManagerScene extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: StatusBar.currentHeight || 0,
   },
-  content: {
-    fontSize: 40,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
